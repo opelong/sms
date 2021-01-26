29876ceb040a65203653f4415393e002656e8b7d
thuto455@gmail.com <thuto455@gmail.com>
2021-01-26 11:16:55 +0200

    First Commit

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="{{url('/')}}"><img src="/images/thumb_7_education.png" class="card-img-top" alt="..."></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+        <a class="nav-link" href="{{url('/')}}">HOME <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="{{url('/create')}}">CREATE</a>
+  </div>
+</nav>
